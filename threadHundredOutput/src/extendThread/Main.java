6379104cc85fbb93c58cc73fca14ed58e2b0e439
Shenglei-Mao <mao59@purdue.edu>
2019-05-14 20:12:00 +0800

    Two thread count to 100 in order

---
+++ threadHundredOutput/src/extendThread/Main.java
@@ -0,0 +1,14 @@
+package extendThread;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		OddThread t1 = new OddThread();
+		EvenThread t2 = new EvenThread();
+		t1.start();
+		t2.start();
+
+	}
+
+}
