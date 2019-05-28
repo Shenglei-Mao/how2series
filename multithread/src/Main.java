978cee59882c602b28081ebf4432b28b2843375e
Shenglei-Mao <mao59@purdue.edu>
2019-05-28 13:24:05 +0800

    learned the java.concurrent.locks.lock

---
+++ multithread/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < 100; i++){
+//            list.add(i);
+//        }
+
+        /*
+        说明iter对于list来说是有序的
+        */
+//        Iterator<Integer> iter = list.iterator();
+//        while (iter.hasNext()){
+//            System.out.println(iter.next());
+//        }
+
+
+//        int num = Integer.parseInt("00");
+//        System.out.println(num != 0);
+
+//        try {
+//            System.out.println("Inside try");
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }finally {
+//            System.out.println("Inside finally");
+//
+//        }
+//        System.out.println("At the end");
+    }
+}
