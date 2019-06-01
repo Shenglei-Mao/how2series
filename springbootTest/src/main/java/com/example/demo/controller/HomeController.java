f595be2c3d317e98eeb4ca3fe6bb207f79baf89c
Shenglei-Mao <mao59@purdue.edu>
2019-06-01 12:57:50 +0800

    learning springboot

--- springbootTest/src/main/java/com/example/demo/controller/HomeController.java
+++ springbootTest/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/home")
 public class HomeController {
 
-    @RequestMapping("/")
-    public String home(){
-        return "<p>Hello World</p>";
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome(){
+        return "welcome";
     }
 }
