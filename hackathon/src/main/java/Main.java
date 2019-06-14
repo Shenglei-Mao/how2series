aad6efa28b4792dc2f4b96903bda37c952e21793
Shenglei Mao <>
2019-06-14 20:31:14 +0800

    chartjs learning

---
+++ hackathon/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeAppPayModel;
+import com.alipay.api.request.AlipayOpenPublicTemplateMessageIndustryModifyRequest;
+import com.alipay.api.request.AlipayTradeAppPayRequest;
+import com.alipay.api.response.AlipayOpenPublicTemplateMessageIndustryModifyResponse;
+import com.alipay.api.response.AlipayTradeAppPayResponse;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.alipay.api.AlipayConstants.APP_ID;
+import static com.alipay.api.AlipayConstants.CHARSET;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
